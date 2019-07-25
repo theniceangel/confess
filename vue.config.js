2a15c574b09d488b3408dd35ee254284ffd159a1
jizhi <jizhi@didichuxing.com>
2019-07-25 20:44:42 +0800

    gh

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
 module.exports = {
-  outputDir: path.resolve(__dirname, 'docs')
+  outputDir: path.resolve(__dirname, 'docs'),
+  publicPath: 'https://theniceangel.github.io/confess/docs/'
 }
