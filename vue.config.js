01de57885de58545f8be1e199143a15ff0ad73ba
jizhi <jizhi@didichuxing.com>
2019-07-26 10:17:32 +0800

    fix assets path

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 const path = require('path')
 module.exports = {
   outputDir: path.resolve(__dirname, 'docs'),
-  publicPath: isProd ? 'https://theniceangel.github.io/confess/docs/' : '/'
+  publicPath: isProd ? 'https://theniceangel.github.io/confess/' : '/'
 }
